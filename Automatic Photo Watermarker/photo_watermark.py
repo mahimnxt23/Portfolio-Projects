6635847ffb2e399d5e712b04b306b150f7f03ec7
mahimnxt23 <mahimnxt23@gmail.com>
2023-12-16 23:29:04 +0600

    Update and Fix

---
+++ Automatic Photo Watermarker/photo_watermark.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# UI setup...
+window = Tk()
+window.minsize(width=720, height=480)
+window.title('Photo Watermarker')
+window.config(padx=20, pady=20)
+
+
+window.mainloop()
