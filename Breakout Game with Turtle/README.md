a475270cf4f5314718f1d4fd0fe1a58cfa0008f0
mahimnxt23 <mahimnxt23@gmail.com>
2023-12-21 01:49:48 +0600

    added Breakout game README and TODOs containing main.py

---
+++ Breakout Game with Turtle/README.md
@@ -0,0 +1,2 @@
+The following project is a clone of the famous Breakout game from my point of view.
+You can play this using the arrow keys on your keyboard or the 'a' and 'd' key.
---
+++ Breakout Game with Turtle/main.py
@@ -0,0 +1,48 @@
+# TODO-1, install required packages...
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - SETUP PARTS - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# TODO-2, setup the game interface...
+
+
+# TODO-3, setup the handle/paddle used to direct the ball...
+
+
+# TODO-4, setup the ball (center of attraction)...
+
+
+# TODO-5, setup the bricks...
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - SETUP PARTS - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - LOGICAL PARTS - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# TODO-6, finish handle/paddle movement functions...
+
+
+# TODO-7, use the functions from TODO-6 to bind keyboard keys for controlling...
+
+
+# TODO-8, make the game loop until game over...
+
+
+# TODO-9, constantly move the ball...
+
+
+# TODO-10, check for border collision and bounce back in...
+
+
+# TODO-11, check ball and handle/paddle collision also set it's deflection direction...
+
+
+# TODO-12, check for brick and ball collision (another main attraction)...
+
+
+# TODO-13, implement and check for scores...
+
+
+# TODO-14, manage force-quit or normal ways to breakout from the loop...
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - LOGICAL PARTS - - - - - - - - - - - - - - - - - - - - - - - - - -
