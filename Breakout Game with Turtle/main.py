cac8ccd4cc7b998362d67858c6629414ef7c0d68
mahimnxt23 <mahimnxt23@gmail.com>
2023-12-23 21:08:33 +0600

    Add breakout game parts

--- Breakout Game with Turtle/main.py
+++ Breakout Game with Turtle/main.py
@@ -1,18 +1,53 @@
 # TODO-1, install required packages...
+import turtle as tim
+import random
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - SETUP PARTS - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # TODO-2, setup the game interface...
+window = tim.Screen()
+window.title('Breakout Cloned with Turtle')
+window.setup(width=480, height=720)
+window.bgcolor('black')
+# window.tracer()
 
 
 # TODO-3, setup the handle/paddle used to direct the ball...
+paddle = tim.Turtle()
+paddle.penup()
+paddle.goto(x=0, y=(-340))
+paddle.color('white')
+paddle.shape('square')
+paddle.shapesize(stretch_wid=1, stretch_len=5)
+# paddle.speed(0)
 
 
 # TODO-4, setup the ball (center of attraction)...
+ball = tim.Turtle()
+ball.penup()
+ball.goto(x=0, y=0)
+ball.color('white')
+ball.shape('circle')
+ball.dx = 2
+ball.dy = -2
+# ball.speed(0)
 
 
 # TODO-5, setup the bricks...
+colors = ['red', 'blue', 'green', 'purple', 'orange']
+bricks = []
+
+for i in range(1):  # originally value was 5...
+    for j in range(6):
+        brick = tim.Turtle()
+        brick.penup()
+        brick.goto((-220 + (j * 90)), (220 - (i * 100)))  # this line is buggy...
+        brick.color(random.choice(colors))
+        brick.shape('square')
+        brick.shapesize(stretch_wid=1, stretch_len=5)
+        # brick.speed(100)
+        bricks.append(brick)
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - SETUP PARTS - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -46,3 +81,5 @@
 # TODO-14, manage force-quit or normal ways to breakout from the loop...
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - LOGICAL PARTS - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+window.mainloop()
