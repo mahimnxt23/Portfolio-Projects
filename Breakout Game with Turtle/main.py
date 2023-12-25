acbbcf44e1a9bf491626d5819965f925c9513a23
mahimnxt23 <mahimnxt23@gmail.com>
2023-12-25 22:39:43 +0600

    completed Breakout clone project with many features

--- Breakout Game with Turtle/main.py
+++ Breakout Game with Turtle/main.py
@@ -4,21 +4,25 @@ from ball import Ball
 from brick import Brick
 from scoreboard import Scoreboard
 from time import sleep
+from tkinter import TclError
 
 
+# UI setup...
 screen = Screen()
 screen.title('Breakout Cloned with Turtle')
 screen.bgcolor('black')
 screen.setup(width=480, height=720)
 screen.tracer(0)
 
+# initiate all packages...
 paddle = Paddle((0, -340))
 ball = Ball()
 score = Scoreboard()
 
 
+# create the bricks...
 bricks = []
-for row in range(6):
+for row in range(8):
     for column in range(11):
         brick = Brick()
 
@@ -29,6 +33,7 @@ for row in range(6):
         bricks.append(brick)
 
 
+# start listening for key presses...
 screen.listen()
 screen.onkey(paddle.paddle_left, 'a')
 screen.onkey(paddle.paddle_left, 'Left')
@@ -36,21 +41,51 @@ screen.onkey(paddle.paddle_right, 'd')
 screen.onkey(paddle.paddle_right, 'Right')
 
 
+total_hits = 0
 game_is_on = True
 while game_is_on:
-    screen.update()
-    ball.move()
 
-    if ball.xcor() < -230 or ball.xcor() > 220:
-        ball.bounce_x()
+    try:
+        screen.update()  # update screen all the time...
+        score.update_scoreboard()  # showcase scoreboard all the time...
+        ball.move()  # start moving the ball...
 
-    if ball.ycor() > 340:
-        ball.bounce_y()
+        if ball.xcor() < -230 or ball.xcor() > 220:  # checks sidebar collision and bounces back in...
+            ball.bounce_x()
 
-    if ball.ycor() < -330 and (paddle.xcor() - 35 < ball.xcor() < paddle.xcor() + 35):
-        ball.bounce_y()
-
-    for block in bricks:
-        if block.distance(ball) < 20 and -360 < block.ycor() < 360:
-            block.disappear()
+        if ball.ycor() > 340:  # checks upper bar collision and bouncing back in...
             ball.bounce_y()
+
+        if ball.ycor() < -340:  # checks down bar collision, deduct life and reset ball position...
+            score.deduct_lives()
+            sleep(0.5)  # adds a pause to react...
+            ball.goto(0, 0)
+            ball.move()  # starting ball movement again...
+
+        # checks collision with paddle and bouncing back up...
+        if ball.ycor() < -330 and (paddle.xcor() - 50 < ball.xcor() < paddle.xcor() + 50):
+            ball.bounce_y()
+
+        if score.remaining_lives == 0:  # checks if Game Over needs to shown...
+            score.show_game_over()
+            # ball.goto(0, 0)
+            # sleep(5)
+            game_is_on = False
+
+        if total_hits == len(bricks):  # checks if Congratulations needs to shown...
+            score.show_congrats()
+            # sleep(3)
+            game_is_on = False
+
+        for block in bricks:  # constantly look for hits with brick and update all proprieties...
+            if block.distance(ball) < 20 and -360 < block.ycor() < 360:
+                score.add_score()
+                total_hits += 1
+                block.disappear()
+                ball.bounce_y()
+
+    except TclError:  # this error just randomly pops up sometimes, so silencing it...
+        pass
+
+
+screen.exitonclick()
--- Breakout Game with Turtle/scoreboard.py
+++ Breakout Game with Turtle/scoreboard.py
@@ -8,20 +8,28 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.hit_score = 0
-        self.remaining_lives = 5
+        self.remaining_lives = 3
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
         self.goto(-100, 300)
-        self.write(self.hit_score, align='center', font=('courier', 25, 'normal'))
+        self.write(self.hit_score, align='center', font=('verdana', 25, 'normal'))
         self.goto(100, 300)
-        self.write(self.remaining_lives, align='center', font=('courier', 25, 'normal'))
+        self.write(self.remaining_lives, align='center', font=('verdana', 25, 'normal'))
 
-    def left_point(self):
+    def add_score(self):
         self.hit_score += 15
         self.update_scoreboard()
 
-    def right_point(self):
+    def deduct_lives(self):
         self.remaining_lives -= 1
         self.update_scoreboard()
+
+    def show_game_over(self):
+        self.goto(0, -150)
+        self.write('Game over!', align='center', font=('verdana', 30, 'normal'))
+
+    def show_congrats(self):
+        self.goto(0, -150)
+        self.write('Congratulations 🎉!', align='center', font=('verdana', 30, 'normal'))
