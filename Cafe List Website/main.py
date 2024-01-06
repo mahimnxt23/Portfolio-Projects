7a806e97a37f389df9fcef0d30f1a1418e06f98c
Mahi-Bi Alam <mahimnxt23@gmail.com>
2024-01-07 00:58:31 +0600

    starting to work on the cafe list website

--- Cafe List Website/main.py
+++ Cafe List Website/main.py
@@ -1,1 +1,14 @@
-pass
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+current_year = datetime.now().year
+
+
+@app.route('/')
+def homepage():
+    return render_template('index.html', year=current_year)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
