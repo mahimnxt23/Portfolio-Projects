7262602d95502f2898ba8f98002e8b7a4a669435
Mahi-Bi Alam <mahimnxt23@gmail.com>
2024-01-25 00:25:00 +0600

    uploaded first impliment of To-Do list website work

--- To-Do List Website/main.py
+++ To-Do List Website/main.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def home_page():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(
-        host='localhost',
-        debug=True
-    )
+    app.run(debug=True)
