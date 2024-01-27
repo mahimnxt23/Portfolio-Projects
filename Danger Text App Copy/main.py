93f3bca8050ff11d701e6f16fd4bd65e5dd638c5
Mahi-Bi Alam <mahimnxt23@gmail.com>
2024-01-28 02:20:23 +0600

    Completed 'Disappearing Text App'

--- Danger Text App Copy/main.py
+++ Danger Text App Copy/main.py
@@ -53,6 +53,7 @@ def save_text():
             text_to_write = user_writen_texts
         else:
             text_to_write = f'\n{user_writen_texts}'
+
         with open('writen_text.txt', 'a') as file:
             file.write(text_to_write)
             user_writen_texts = ''
@@ -62,12 +63,11 @@ def save_text():
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - STYLING - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-BORDER = '#3C2C3E'
-FG = 'khaki'
-BG = '#4B5D67'
+BORDER = '#93B1A6'
+FG = '#9EC8B9'
+BG = '#030637'
 
-FONT_FAMILY1 = 'Calibri'
-FONT_FAMILY2 = 'Helvetica'
+FONT_FAMILY = 'consolas'
 
 FONT_SIZE1 = 14
 FONT_SIZE2 = 18
@@ -77,37 +77,40 @@ FONT_STYLE1 = 'normal'
 FONT_STYLE2 = 'italic'
 FONT_STYLE3 = 'bold'
 
-PARA_FONT = (FONT_FAMILY1, FONT_SIZE1, FONT_STYLE3)
-PARA_FONT2 = (FONT_FAMILY1, 12, FONT_STYLE2)
-HEAD_FONT = (FONT_FAMILY2, FONT_SIZE3, FONT_STYLE1)
+BASE_FONT = (FONT_FAMILY, FONT_SIZE1, FONT_STYLE3)
+INSTRUCTION_FONT = (FONT_FAMILY, 12, FONT_STYLE2)
+HEADING_FONT = (FONT_FAMILY, FONT_SIZE3, FONT_STYLE1)
 
 heading = 'CONSIDER WRITING WITH MAGICAL INK'
-instruction = 'If you don\'t press any key for 5 seconds, the text you have written will disappear'
+instruction = '( If you don\'t press any key for 5 seconds, the text you have written will disappear )'
 # - - - - - - - - - - - - - - - - - - - - - - - - - STYLING - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
 # UI setup...
 window = Tk()
 window.title('Text Disappearing App')
-window.minsize(width=640, height=360)
-window.configure(bg=BG, padx=20, pady=10)
+window.minsize(width=1060, height=550)
+window.maxsize(width=1060, height=550)
+window.configure(bg=BG, padx=20, pady=20)
 
-# using 'consolas' font for Labels and Buttons...
-window.option_add(pattern='*Label.Font', value='consolas 30')
-window.option_add(pattern='*Button.Font', value='consolas 30')
 
-heading_label = Label(text=heading, font=HEAD_FONT, bg=BG, fg=FG, padx=10, pady=10)
-instruction_label = Label(text=instruction, font=PARA_FONT2, bg=BG, fg=FG, pady=10)
+# labels...
+heading_label = Label(text=heading, font=HEADING_FONT, bg=BG, fg=FG, padx=10, pady=10)
+instruction_label = Label(text=instruction, font=INSTRUCTION_FONT, bg=BG, fg=FG, pady=10)
 
-typing_area = Text(font=PARA_FONT, bg=BG, fg=FG, width=100, height=15, wrap='w', highlightcolor=BORDER,
+# text areas...
+typing_area = Text(font=BASE_FONT, bg=BG, fg=FG, width=100, height=15, wrap='w', highlightcolor=BORDER,
                    highlightthickness=4, highlightbackground=BORDER, padx=5, pady=5)
 typing_area.bind('<KeyPress>', start_calculating)
 
-reset_button = Button(text='Reset Everything!', fg=FG, bg=BG, font=PARA_FONT, border=3, highlightbackground=FG,
-                      highlightcolor=FG, highlightthickness=0, width=50, command=reset_app)
-save_button = Button(text='Save!', fg=FG, bg=BG, font=PARA_FONT, border=3, highlightbackground=FG, width=50,
-                     highlightcolor=FG, highlightthickness=0, command=save_text)
+# buttons...
+reset_button = Button(text='Reset Everything!', fg=FG, bg=BG, font=BASE_FONT, border=3, highlightbackground=FG,
+                      highlightcolor=FG, highlightthickness=0, width=50, pady=5, command=reset_app)
+save_button = Button(text='Save!', fg=FG, bg=BG, font=BASE_FONT, border=3, highlightbackground=FG, width=50,
+                     highlightcolor=FG, highlightthickness=0, pady=5, command=save_text)
 
+
+# UI placements...
 heading_label.grid(row=0, column=0, columnspan=3)
 instruction_label.grid(row=2, column=0, columnspan=3)
 typing_area.grid(row=3, column=0, columnspan=3)
@@ -115,4 +118,4 @@ reset_button.grid(row=4, column=0)
 save_button.grid(row=4, column=2)
 
 
-window.mainloop()
+window.mainloop()  # keeping the window on loop...
