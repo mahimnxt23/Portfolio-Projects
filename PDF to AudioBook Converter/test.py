182cac070e1131fe2f903853ec42620e3af7e392
Mahi-Bi Alam <mahimnxt23@gmail.com>
2024-01-29 01:37:34 +0600

    MISC update for 'PDF to AudioBook'

---
+++ PDF to AudioBook Converter/test.py
@@ -0,0 +1,23 @@
+# this one is for scripting purpose only!
+
+from PyPDF2 import PdfReader
+from pyttsx3 import init
+import pdfplumber as pdp
+
+file_to_convert = 'sample.pdf'
+
+pdf_object = open(file_to_convert, mode='rb')  # making everything in readable binary format...
+pdf_reader = PdfReader(pdf_object)
+num_of_pages = len(pdf_reader.pages)  # knowing total pages available...
+
+
+with pdp.open(file_to_convert) as pdf:
+
+    for index in range(num_of_pages):
+        page = pdf.pages[index]
+        text = page.extract_text()  # extracting texts from the pdf page on current index...
+        print(text)
+
+        speaker = init()  # initializing pytts speaker engine...
+        speaker.say(text)  # making it say while reading...
+        speaker.runAndWait()  # holding it till everything is narrated...
