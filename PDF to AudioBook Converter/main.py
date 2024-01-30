b75f2c72033968ef915539a501764bada71eb50f
Mahi-Bi Alam <mahimnxt23@gmail.com>
2024-01-31 01:02:39 +0600

    Adding function update to PDF to Audiobook app

--- PDF to AudioBook Converter/main.py
+++ PDF to AudioBook Converter/main.py
@@ -2,3 +2,9 @@ from tkinter import filedialog
 from PyPDF2 import PdfReader
 from pyttsx3 import init
 import pdfplumber as pdp
+
+
+def get_file():
+    file = filedialog.askopenfilename(filetypes=[('PDF files', '*.pdf')])
+    return file
+
