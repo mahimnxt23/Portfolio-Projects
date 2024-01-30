661c3828d7facc51a22060462de75a1fd28b5ee3
Mahi-Bi Alam <mahimnxt23@gmail.com>
2024-01-31 01:03:10 +0600

    Adding function update to PDF to Audiobook app

--- PDF to AudioBook Converter/main.py
+++ PDF to AudioBook Converter/main.py
@@ -8,3 +8,12 @@ def get_file():
     file = filedialog.askopenfilename(filetypes=[('PDF files', '*.pdf')])
     return file
 
+
+def read_from_file():
+    desired_file = get_file()
+
+    pdf_object = open(desired_file, mode='rb')  # making everything in readable binary format...
+    pdf_reader = PdfReader(pdf_object)
+    num_of_pages = len(pdf_reader.pages)  # knowing total pages available...
+
+
