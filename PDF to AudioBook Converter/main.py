0647ccbcd4f028ca7be62d667e43282977545a4f
Mahi-Bi Alam <mahimnxt23@gmail.com>
2024-01-31 01:13:44 +0600

    Adding function update to PDF to Audiobook app

--- PDF to AudioBook Converter/main.py
+++ PDF to AudioBook Converter/main.py
@@ -29,10 +29,10 @@ def read_from_file():
 
 def speak_text():
     speaker = init()  # initializing pytts speaker engine...
-    speech_rate = speaker.getProperty('rate')
-    voice_property = speaker.getProperty('voices')
+    # speech_rate = speaker.getProperty('rate')
+    # voice_property = speaker.getProperty('voices')
 
-    print(speech_rate)
+    # print(speech_rate)
     speaker.setProperty('rate', 275)
     # speaker.setProperty('voice', voice_property[0].id)  # for male voice...
     # speaker.setProperty('voice', voice_property[1].id)  # for female voice...
