c0645ff4992c051ae68cfaea2958118db471d79d
MaHim <mahimnxt23@gmail.com>
2024-02-22 10:33:14 -0800

    misc update for Web Scrapper Project

--- Web Scrapper Project/main.py
+++ Web Scrapper Project/main.py
@@ -1,19 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
-res = requests.get('https://news.ycombinator.com/news')
-soup = BeautifulSoup(res.text, 'html.parser')
+res = requests.get("https://news.ycombinator.com/news")
+soup = BeautifulSoup(res.text, features="html.parser")
 
 # print(soup.body.prettify())
 # posts = soup.select('.titleline')
 
-posts = soup.findAll(name='span', attrs={'class': 'titleline'})
-votes = soup.findAll(name='span', attrs={'class': 'score'})
+posts = soup.findAll(name="span", attrs={"class": "titleline"})
+links = soup.select("span > a")
+votes = soup.findAll(name="span", attrs={"class": "score"})
 
 # votes = soup.select('.score')
 
 # print(posts[0])
 # print(votes[0])
+# print(links)
+
+for link in links:
+    print(link.get("href"))
 
 
 def run_scrapper():
@@ -22,11 +28,11 @@ def run_scrapper():
     for index, item in enumerate(posts):
         title = posts[index].getText()
         # href = posts[index].find_next_sibling('a').get('href', None)
-        href = posts[index].select('.titleline ~ a').get('href', None)
+        href = links[index].get("href", None)
         # points = votes[index].getText()
 
-        hot_news.append({'title': title, 'link': href})
+        hot_news.append({"title": title, "link": href})
     return hot_news
 
 
-print(run_scrapper())
+# print(run_scrapper())
