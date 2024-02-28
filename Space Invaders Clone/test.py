8faffe24d326dda80c4fe0aa8fd7680c162e470f
MaHim <mahimnxt23@gmail.com>
2024-02-29 00:40:14 +0600

    initial commit for Space Invaders Clone

---
+++ Space Invaders Clone/test.py
@@ -0,0 +1,10 @@
+import turtle as tim
+
+# window setup...
+wn = tim.Screen()
+wn.setup(width=540, height=540)
+wn.bgcolor("lightgreen")
+wn.title("Space Invaders")
+# wn.tracer(0)
+
+# drawing border...
