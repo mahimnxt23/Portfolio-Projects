7a76f5e5748ba42472f3cf552d3f7471bc5c849a
MaHim <mahimnxt23@gmail.com>
2024-04-24 23:58:49 +0600

    updates for Keeper project

--- react-related/Google Keeper Clone/src/components/App.jsx
+++ react-related/Google Keeper Clone/src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(noteContent => (
+                <Note 
+                    key={noteContent.id}
+                    title={noteContent.title}
+                    content={noteContent.content}
+                />
+                )
+            )}
             <Footer />
         </div>
     );
